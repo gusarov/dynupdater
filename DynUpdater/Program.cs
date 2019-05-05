4c96295e4156e4c9e59f9fbee5856d02b36a73f4
xkip <1462777+gusarov@users.noreply.github.com>
2019-05-05 14:54:08 -0400

    windows service

--- DynUpdater/Program.cs
+++ DynUpdater/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
 using System.Net;
+using System.Reflection;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,17 +14,32 @@ namespace DynUpdater
 	{
 		static void Main(string[] args)
 		{
-			var utf8 = new UTF8Encoding(false, false);
-			var user = args[0];
-			var password = args[1];
-			var net = args[2];
-			var cred = user + ":" + password;
-			cred = Convert.ToBase64String(utf8.GetBytes(cred));
+			var svc = new TheWindowsService();
+			if (args.Any(x => string.Equals("-service", x, StringComparison.OrdinalIgnoreCase)))
+			{
+				ServiceBase.Run(new ServiceBase[] { svc });
+			}
+			else if (args.Any(x => string.Equals("-install", x, StringComparison.OrdinalIgnoreCase)))
+			{
+				ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+			}
+			else if (args.Any(x => string.Equals("-uninstall", x, StringComparison.OrdinalIgnoreCase)))
+			{
+				ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+			}
+			else
+			{
+				Console.WriteLine(" -eventlog: produce fake event log for triggers");
+				Console.WriteLine(" -service: use to run as windows service");
+				Console.WriteLine(" -install: install windows service");
+				Console.WriteLine(" -uninstall: uninstall windows service");
+				Console.WriteLine("Running as a console applicaiton...");
+				svc.StartImpl(args);
+				Console.WriteLine("Press any key to stop . . .");
+				Console.ReadKey();
+				svc.StopImpl();
+			}
 
-			var webClient = new WebClient();
-			webClient.Headers[HttpRequestHeader.Authorization] = " Basic " + cred;
-			var data = webClient.DownloadString($"https://updates.opendns.com/nic/update?hostname={net}");
-			Console.WriteLine(data);
 		}
 	}
 }
---
+++ DynUpdater/TheWindowsService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace DynUpdater
+{
+	partial class TheWindowsService : ServiceBase
+	{
+		public TheWindowsService()
+		{
+			InitializeComponent();
+		}
+
+		string[] _args;
+		protected override void OnStart(string[] args)
+		{
+			// TODO: Add code here to start your service.
+			StartImpl(args);
+		}
+
+		protected override void OnStop()
+		{
+			// TODO: Add code here to perform any tear-down necessary to stop your service.
+			StopImpl();
+		}
+
+		Timer _timer;
+
+		public void StartImpl(string[] args)
+		{
+			_args = args;
+
+			_timer = new Timer(Worker, null, 1, 5 * 60 * 1000);
+		}
+
+		string lastIp;
+
+		private void Worker(object state)
+		{
+			// get ip
+			string ip;
+			using(var webClient1 = new WebClient())
+			{
+				ip = webClient1.DownloadString("https://api.ipify.org/");
+				if (ip == lastIp)
+				{
+					Console.WriteLine($"No update neede. IP = {ip}");
+					return;
+				}
+			}
+
+			var utf8 = new UTF8Encoding(false, false);
+			var user = _args[1];
+			var password = _args[2];
+			var net = _args[3];
+			var cred = user + ":" + password;
+			cred = Convert.ToBase64String(utf8.GetBytes(cred));
+
+			var webClient = new WebClient();
+			webClient.Headers[HttpRequestHeader.Authorization] = " Basic " + cred;
+			var data = webClient.DownloadString($"https://updates.opendns.com/nic/update?hostname={net}");
+			Console.WriteLine(data);
+			File.AppendAllText("C:\\Temp\\log.txt", DateTime.Now + " " + data);
+			if (data.ToLowerInvariant().StartsWith("good "))
+			{
+				// remember IP
+				Console.WriteLine("IP remembered: " + ip);
+				lastIp = ip;
+			}
+		}
+
+		public void StopImpl()
+		{
+
+		}
+	}
+}
